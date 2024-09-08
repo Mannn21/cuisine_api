9e7b62f8344c9ed07a27dde412e1a8c244f9d340
Mannn21 <aimannurrofi21@gmail.com>
2024-09-08 12:03:31 +0700

    feat: add user api and update get all users api

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,25 +1,37 @@
 import { initializeApp as initializeClientApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import * as admin from 'firebase-admin';
-import serviceAccount from '../config/cuisine-6ff3f-firebase-adminsdk-lql7w-2ec6bfc376.json'; // Pastikan path ini benar
+import { CustomServiceAccount, FirebaseConfigInterface } from './interfaces/FirebaseConfigInterface';
 
-const serviceAccountCred = serviceAccount as admin.ServiceAccount;
+const serviceAccountCred: CustomServiceAccount = {
+  type: process.env.NODE_FIREBASE_TYPE as string,
+  project_id: process.env.NODE_FIREBASE_PROJECT_ID as string,
+  private_key_id: process.env.NODE_FIREBASE_PRIVATE_KEY_ID as string,
+  private_key: (process.env.NODE_FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
+  client_email: process.env.NODE_FIREBASE_CLIENT_EMAIL as string,
+  client_id: process.env.NODE_FIREBASE_CLIENT_ID as string,
+  auth_uri: process.env.NODE_FIREBASE_AUTH_URI as string,
+  token_uri: process.env.NODE_FIREBASE_TOKEN_URI as string,
+  auth_provider_x509_cert_url: process.env.NODE_FIREBASE_AUTH_PROVIDER_X509_CERT_URL as string,
+  client_x509_cert_url: process.env.NODE_FIREBASE_CLIENT_X509_CERT_URL as string,
+  universe_domain: process.env.NODE_FIREBASE_UNIVERSE_DOMAIN as string,
+};
 
-const firebaseConfig = {
-  apiKey: process.env.NODE_FIREBASE_API_KEY,
-  authDomain: process.env.NODE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NODE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NODE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NODE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NODE_FIREBASE_APP_ID,
-  measurementId: process.env.NODE_FIREBASE_MEASUREMENT_ID
+const firebaseConfig: FirebaseConfigInterface = {
+  apiKey: `${process.env.NODE_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.NODE_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${process.env.NODE_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.NODE_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.NODE_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.NODE_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.NODE_FIREBASE_MEASUREMENT_ID}`
 };
 
 const app = initializeClientApp(firebaseConfig);
 export const db = getFirestore(app);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountCred),
+  credential: admin.credential.cert(serviceAccountCred as admin.ServiceAccount),
 });
 
 export const adminDb = admin.firestore();
